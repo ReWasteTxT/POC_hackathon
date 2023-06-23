7567f47ccc3e4081f1bab8495449ed8f99990bff
Michele Rago <michele.rago@network.txtgroup.com>
2023-06-24 01:49:02 +0200

    Init Commit

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace POC_hackathon;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace POC_hackathon;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
